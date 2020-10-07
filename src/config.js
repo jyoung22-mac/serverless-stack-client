44e880ad6cd97a09f099eeaf7aa858e05252054f
Justin Young <justinyoung371@yahoo.com>
2020-10-07 11:30:18 -0700

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "justin-young-notes-app-uploads"
+    },
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: "https://idd8826bug.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_EuP0l1Dco",
+      APP_CLIENT_ID: "4pub1sehpb4qto7p10v269qp3a",
+      IDENTITY_POOL_ID: "us-east-1:99dcb347-0a5d-43b2-a527-ed2957c1d25d"
+    }
+  };
